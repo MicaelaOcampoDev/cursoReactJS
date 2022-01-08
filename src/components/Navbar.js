aa6c0e3d51229417b8cfc8cb293e2edd2507ca9b
Micaela Ocampo <ocampomica1809@gmail.com>
2022-01-08 11:41:22 -0300

    cree navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar(){
+    return <nav class="Navbar">PACHITA CO.  <h1>Tienda de productos sustentables!</h1>
+      <ul>
+        <li class="Style1"><a href="https://www.instagram.com/pachita.co/"> Home </a></li>
+        <li class="Style"><a href="https://www.instagram.com/pachita.co/"> Tienda </a></li>
+        <li class="Style"><a href="https://www.instagram.com/pachita.co/"> Quienes somos </a></li>
+        <li class="Style"><a href="https://www.instagram.com/pachita.co/"> Referencias</a></li>
+      </ul>
+    </nav>
+    
+}
+
+export default Navbar;
